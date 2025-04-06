64e31168f64949c40e0ae031b9601de67b9787ae
ynesterenk <yevgen2005@gmail.com>
2025-04-06 16:17:24 +0300

    FINANCE-001: Replace CurrencyExchangeRate with CrossAssets

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CrossAssets from './CrossAssets';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container!);
 root.render(
   <React.StrictMode>
     <App />
+    <CrossAssets />
   </React.StrictMode>
 );
 
