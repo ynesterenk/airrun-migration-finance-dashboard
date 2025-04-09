d1c2f622c7c723c258149f5281761543a0b42cc2
ynesterenk <yevgen2005@gmail.com>
2025-04-09 21:24:05 +0300

    REFIDMOPMO-4: Add data source for CrossAssetMonitor plugin

--- src/plugins/CrossAssetMonitor/data.ts
+++ src/plugins/CrossAssetMonitor/data.ts
@@ -8,3 +8,19 @@ export const crossAssetData = [
 ];
 
 export type CrossAssetItem = typeof crossAssetData[0];
+
+export interface AssetItem {
+    asset: string;
+    price: number;
+    dayChange: number;
+    percentChange: number;
+}
+
+export const assetData: AssetItem[] = [
+    { asset: 'NASDAQ 100', price: 15010.43, dayChange: 0.0, percentChange: 0.0 },
+    { asset: 'FTSE 100 INDEX', price: 7455.68, dayChange: -0.14, percentChange: -0.14 },
+    { asset: 'HANG SENG INDEX', price: 16993.44, dayChange: -2.08, percentChange: -2.08 },
+    { asset: 'VANGUARD S&P 500 ETF', price: 511.14, dayChange: 1.08, percentChange: 1.08 },
+    { asset: 'DAX INDEX', price: 22539.61, dayChange: 3.08, percentChange: 3.08 },
+    { asset: 'CAC 40 INDEX', price: 7876.36, dayChange: -2.18, percentChange: -2.18 }
+];
