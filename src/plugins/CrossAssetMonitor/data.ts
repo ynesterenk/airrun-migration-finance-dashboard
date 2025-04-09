a8c8a5b0e6d69964066cba8505c575248e7a2496
ynesterenk <yevgen2005@gmail.com>
2025-04-09 20:55:13 +0300

    REFIDMOPMO-4: Add mock data for CrossAssetMonitor plugin

--- src/plugins/CrossAssetMonitor/data.ts
+++ src/plugins/CrossAssetMonitor/data.ts
@@ -8,3 +8,17 @@ export const crossAssetData = [
 ];
 
 export type CrossAssetItem = typeof crossAssetData[0];
+
+export interface AssetItem {
+    asset: string;
+    price: number;
+    dayChange: number;
+    percentChange: number;
+}
+
+export const mockAssetData: AssetItem[] = [
+    { asset: "Gold", price: 1800.50, dayChange: 10.75, percentChange: 0.60 },
+    { asset: "Silver", price: 25.30, dayChange: 0.20, percentChange: 0.80 },
+    { asset: "Oil", price: 70.45, dayChange: -1.30, percentChange: -1.80 },
+    { asset: "Bitcoin", price: 45000.00, dayChange: 1500.00, percentChange: 3.45 }
+];
