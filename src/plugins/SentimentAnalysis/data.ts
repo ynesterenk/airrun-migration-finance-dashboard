a9bc51025d54f2f1cba5e9780d9e51e461f986fe
ynesterenk <yevgen2005@gmail.com>
2025-05-08 12:13:35 +0300

    REFIDMOPMO-4: Add sentiment data

---
+++ src/plugins/SentimentAnalysis/data.ts
@@ -0,0 +1,10 @@
+export const sentimentData = [
+  { asset: 'S&P 500', sentiment: 0.75 },
+  { asset: 'NASDAQ', sentiment: 0.82 },
+  { asset: 'Dow Jones', sentiment: 0.68 },
+  { asset: 'Russell 2000', sentiment: -0.12 },
+  { asset: 'Bitcoin', sentiment: 0.45 },
+  { asset: 'Gold', sentiment: 0.23 },
+  { asset: 'Crude Oil', sentiment: -0.35 },
+  { asset: 'EUR/USD', sentiment: 0.15 },
+];
